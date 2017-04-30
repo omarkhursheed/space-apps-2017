ad5fd56695652e5f58f5a4c9825c9e38acfb1297
Avi x <axios47@gmail.com>
2017-04-30 07:58:28 +0530

    Ui Progress =80%

--- landWatch/src/app/app.module.ts
+++ landWatch/src/app/app.module.ts
@@ -2,17 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { VisualComponent } from './visual/visual.component';
+import { NewsComponent } from './news/news.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EntryComponent } from './entry/entry.component';
+
+var homePath:string;
+export var login:boolean=true;
+if(login){
+  homePath='visual';
+}else {
+  homePath='welcome';
+}
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: EntryComponent },
+  { path: 'visual', component: VisualComponent },
+  { path: 'news', component: NewsComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: homePath,
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualComponent,
+    NewsComponent,
+    PageNotFoundComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
